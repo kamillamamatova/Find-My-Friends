7113700121c4b9be69d744f9ac830ceb9e29dbf8
kamillamamatova <kamillamamatova.km@gmail.com>
2025-03-20 18:48:07 -0400

    Update find_my_friends.py

--- find_my_friends.py
+++ find_my_friends.py
@@ -5,3 +5,7 @@ KLong = float(KLocation[1])
 ALocation = ('40.963987', '-75.960642')
 ALat = float(ALocation[0])
 ALong = float(ALocation[1])
+
+VLocation = ('27.947420', '-82.458778')
+VLat = float(VLocation[0])
+VLong = float(VLocation[1])
